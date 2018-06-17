ecdccbbffdf58423b21c6f03299d417d9355f9ec
sgattani <saurabh.gattani@gmail.com>
2018-06-17 19:29:41 +0530

    work in progress. Modify tests-spec.js and add karma watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,41 @@
 module.exports = function(grunt) {
 
+var karma = require('karma');
+var kbg = require('karma-background');
+var karmaPath = __dirname + '\\config\\karma-conf.js';
+
   grunt.initConfig({
 	  
 	server: {
        port: 8000,
        base: './'
     },
+	bgShell: {
+      _defaults: {
+        bg: true
+      },
+      runTests: {
+		cmd: 'grunt unit-tests',
+        bg: false 
+	  },
+	  runServer: {
+		  cmd: 'grunt server',
+		  bg: false
+	  }
+    },
+	
+	/*karma: {
+		  options: {
+			configFile: 'config/karma-conf.js'
+		  },
+		  unit: {
+			singleRun: true
+		  },
+		continuous: {
+			// keep karma running in the background
+			background: true
+		}
+	},*/
 	
     jshint: {
       files: ['Gruntfile.js', 'tests/*.js'],
@@ -95,6 +125,11 @@ module.exports = function(grunt) {
       },
       options: {
         livereload: true
+      },
+	  karma: {
+        // run these tasks when these files change
+        files: ['src/js/*.js', 'tests/*.js'],
+        tasks: ['karma:run'] // note the :run flag
       }
     },
     clean: {
@@ -132,6 +167,7 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-clean');
@@ -140,9 +176,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-bg-shell');
 
   //default task
   grunt.registerTask('default', [
+	  'bgShell',
       'clean:tmp',
       'clean:dev',
       'jshint',
@@ -150,10 +188,10 @@ module.exports = function(grunt) {
       'sass:dev',
       'concat:devcss',
       'copy:devviews',
-      'copy:devtemplates',
-	  'server'
+      'copy:devtemplates'
   ]);
   grunt.registerTask('prod', [
+	  'bgShell',
       'clean:tmp',
       'clean:prod',
       'jshint',
@@ -161,10 +199,44 @@ module.exports = function(grunt) {
       'sass:dev',
       'cssmin:prod',
       'copy:prodviews',
-      'copy:prodtemplates',
-	  'server'
+      'copy:prodtemplates'
   ]);
   
+grunt.registerTask('unit-tests', ['karma:start', 'karma:watch']);
+
+grunt.registerTask('karma:start', function(done) {
+	new karma.Server({
+		configFile: karmaPath,
+		singleRun: true,
+		autoWatch: true,
+		background: true
+	},
+	done).start();
+});
+
+/**
+ * Runs test runner..
+ */
+/*grunt.registerTask('karma:run', function (done) {
+	console.log("RUNNING karma:run");
+	karma.runner.run({
+		configFile: karmaPath
+	}, done);
+	console.log("Please wait until server starts on port 8000");
+});*/
+
+/**
+ * Starts karma server
+ */
+grunt.registerTask('karma:watch', function () {
+  kbg({
+    configFile: karmaPath
+  });
+
+  grunt.task.run('watch:karma');
+  console.log("Running TESTS !! Please wait until server starts on port 8000 after task runServer finishes");
+});
+
 grunt.registerTask('server', 'Start a custom web server', function() {
     grunt.log.writeln('Started web server on port 8000');
 	var done = this.async();
